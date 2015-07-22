e8471c3836ee16485700c40c70c7b4ea74560c89
Dustin Brothers <rdustinb@users.noreply.github.com>
2015-07-22 11:28:15 -0600

    Randomized tb.sv
    This set of changes alters the simulation to always use different stimulus values between runs. This is in correlation to the changes just made to the makefile to support random seeds used in SV.

--- src/ops/insert_sort/tb.sv
+++ src/ops/insert_sort/tb.sv
@@ -2,12 +2,8 @@
 
 // Parameters
 localparam VALCOUNT = 16;
-localparam VALBIT = 32;
-
-// Random Value Class
-class rand_val;
-  rand logic [VALBIT-1:0] value;
-endclass
+localparam VALBIT = 6;
+localparam RSTPOL = 0;
 
 // Simulation Module
 module tb;
@@ -15,8 +11,15 @@ module tb;
   logic clk;
   logic rst;
   logic sortstart;
+  logic sortdone;
+  logic finish_status;
   logic [(VALCOUNT-1):0][(VALBIT-1):0] values;
-  rand_val single_value [(VALCOUNT-1):0];
+  logic [(VALCOUNT-1):0][(VALBIT-1):0] sorted_values;
+  logic [(VALCOUNT-1):0][$clog2(VALCOUNT):0] sorted_positions;
+  logic [VALBIT-1:0] single_value [(VALCOUNT-1):0];
+
+  logic [63:0] clock_counter;
+  logic counting;
 
   // Clock and Reset
   initial begin
@@ -32,7 +35,7 @@ module tb;
 
   // Instance
   insert_sort #(
-      .RSTPOL             (0),
+      .RSTPOL             (RSTPOL),
       .INPUTVALS          (VALCOUNT),
       .INPUTBITWIDTHS     (VALBIT)
     )insert_sort_0(
@@ -40,33 +43,91 @@ module tb;
       .reset              (rst),
       .sortstart          (sortstart),
       .needs_sorting      (values),
-      .sortdone           (),
-      .sorted             (),
-      .sorted_positions   (),
+      .sortdone           (sortdone),
+      .sorted             (sorted_values),
+      .sorted_positions   (sorted_positions),
       .error              ()
     );
+
+  // Clock Cycle Counter
+  always@(posedge clk) begin
+    if(rst == RSTPOL) begin
+      clock_counter <= 0;
+      counting <= 0;
+    end
+    else begin
+      // Flag to count or not count
+      if(sortstart) begin
+        counting <= 1;
+        clock_counter <= 0;
+      end
+      else if(sortdone) begin
+        counting <= 0;
+      end
+      // Counting
+      if(counting == 1) begin
+        clock_counter <= clock_counter + 1;
+      end
+    end
+  end
 
   // SIM
   initial begin
     sortstart <= 0;
     #1us;
 
+    // Print the current time
+    $display("System >>>> Current time integer is: %d", $system("date +%N"));
+
     // Create a list of random values
     for(int i=0; i<VALCOUNT; i++) begin
-      single_value[i] = new();
-      single_value[i].randomize();
+      single_value[i] = ($urandom_range(0,(2**VALBIT - 1)) + 1) % (2**VALBIT);
       // Assign the random value to the array of values
       values[i] = VALBIT'(single_value[i]);
     end
     // Display the values
+    $display("Original values are:::");
     for(int i=0; i<VALCOUNT; i++) begin
-      $display("0x%x",values[i]);
+      $display("%d",values[i]);
     end
 
     @(posedge clk) sortstart <= 1;
     @(posedge clk) sortstart <= 0;
-    #1us;
+
+    // Wait for sorting to be complete
+    @(posedge sortdone) #1us;
+
+    // Print out the sorted values
+    $display("Sorted values are:::");
+    for(int i=0; i<VALCOUNT; i++) begin
+      $display("%d",sorted_values[i]);
+    end
+    // Print out the sorted positions
+    $display("Sorted positions are:::");
+    for(int i=0; i<VALCOUNT; i++) begin
+      $display("%d",sorted_positions[i]);
+    end
+
     $finish;
+  end
+
+  // Watchdog
+  initial begin
+    finish_status = 0;
+    #100us;
+    finish_status = 1;
+    $finish;
+  end
+
+  // Final Step
+  final begin
+    if(finish_status == 1) begin
+      $display("Results >>>> Simulation did not complete normally.");
+    end
+    else begin
+      $display("Results >>>> Sorting took %d Clock Cycles", clock_counter);
+      $display("Results >>>> Simulation completed normally.");
+    end
   end
 
 endmodule

282dbd1f7af7d91a8ce16c6fcbffbe719cc26950
Dustin B <dustin.brothers@icloud.com>
2015-10-11 17:09:17 -0600

    Documentation of selection_sort

--- src/ops/selection_sort/selection_sort.sv
+++ src/ops/selection_sort/selection_sort.sv
@@ -1,3 +1,29 @@
+/*****************************************************************************
+  Selection Sort Algorithm
+
+Simplistic sorting algorithm. Sorting time is O(n^2). While faster than the
+bubble and gnome sorting algorithms, it is much slower than the better
+optimized insert-sort algorithm (O(n^2) vs. O(n)), however the selection sort
+algorithm has much less write intensity than the insert sort algorithm (𝜃(n) 
+swaps versus O(n^2) swaps). The reason is that the selection sort algorithm
+first FINDS the element that is to be the next in the sorted list, and THEN
+writes it to the ordered list. While the Insert Sort algorithm swaps two
+adjacent numbers within the working list until achieving the final sorted
+list.
+
+This algorithm works by initiating with two lists, one is unsorted (contains
+all original numbers), one is sorted (contains no numbers). The sorting
+algorithm then scans through the list of unsorted numbers looking for the
+smallest (or largest) element in the list and appending it to the sorted
+numbers list.
+
+https://en.wikipedia.org/wiki/Selection_sort
+
+---- Analytics ----
+> Low Memory Writes
+> Slow Execution Speed
+*****************************************************************************/
+
 module selection_sort #(
     parameter RSTPOL                                    = 0,
     parameter INPUTVALS                                 = 16,

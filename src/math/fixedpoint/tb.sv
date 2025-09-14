6f3880c046534aac82b41e0c7364f59732dadc78
Dustin Brothers <gentian.limited-0t@icloud.com>
2025-09-13 18:31:55 -0600

    Saving my work...

--- src/math/fixedpoint/tb.sv
+++ src/math/fixedpoint/tb.sv
@@ -19,6 +19,8 @@ module tb;
   logic[(wholeWidth+fractionWidth)-1:0] difference;
   logic[(wholeWidth+fractionWidth)-1:0] product;
 
+  logic[(wholeWidth+fractionWidth)-1:0] working;
+
   // Clocks and Resets
   initial begin
     clock = 1'b0;
@@ -65,6 +67,55 @@ module tb;
   );
 
   // Tasks
+  task automatic real2hex(
+    input real thisFloat,
+    output[(wholeWidth+fractionWidth)-1:0] thisHex
+  );
+    // The only purpose of this task is for converting starting real numbers to hex.
+    // Don't try to store real values in RTL
+
+    real originalFloat = thisFloat;
+    int originalWhole = int'(thisFloat);
+    real originalFraction = thisFloat-real'(int'(thisFloat));
+
+    int thisFractionTransformInt;
+    int thisHexFractionDigit = 1;
+
+    // Debug (To answer the question: am I stupid or not?)
+    $display("Original float: %.04f\nWhole number: %d\nFractional number: %.04f",
+      originalFloat,
+      originalWhole,
+      originalFraction
+    );
+
+    // First assign the whole number to the hex field
+    thisHex[fractionWidth+:wholeWidth] = wholeWidth'(originalWhole);
+
+    // Now process the fractional part using the following algorithm:
+    // 1) Multiply by 16
+    // 2) Take whole number digit, convert to hex
+    // 3) Append to running hex fraction value
+    // 4) Goto step 1 if the original fraction value is non-zero
+    while(originalFraction != 0 && thisHexFractionDigit <= (fractionWidth / 4)) begin
+      originalFraction = originalFraction * 16;
+      thisFractionTransformInt = int'(originalFraction);
+    end
+
+  endtask
+
+  task automatic hex2real(
+    input[31:0] thisHexFraction,
+    output int  thisDecFraction
+  );
+    // The only purpose of this task is for printing stuff out.
+    // Don't try to store real values in RTL
+    int currentPosition = 10;
+
+    // Loop through each hex digit
+    for(int thisHexDigit = 7; thisHexDigit >= 0; thisHexDigit--) begin
+      // TODO, I'm not sure how to do this yet...
+    end
+  endtask
 
   // Floating Point
   // See IEEE754
@@ -82,6 +133,9 @@ module tb;
   // -4.9999996 to 4.9999996 (0.0000004 precision)
   // -7.9999995 to 4.9999995 (0.0000004 precision)
 
+  //`define TESTRTL
+  `define TESTTASKS
+
   // Simulation
   initial begin
     calculate_en = 1'b0;
@@ -90,30 +144,36 @@ module tb;
 
     #20ns;
 
-    // Run Calculations
-    @(negedge clock);
-    calculate_en = 1'b1;
-    // h0001.h0001
-    valueOne[fractionWidth+:wholeWidth] = 'd3;
-    valueOne[0+:fractionWidth] = 'd1137;
-    // h0001.h0001
-    valueTwo[fractionWidth+:wholeWidth] = 'd16;
-    valueTwo[0+:fractionWidth] = 'd4173;
-    @(negedge clock);
-    calculate_en = 1'b0;
+    `ifdef TESTRTL
+      // Run Calculations
+      @(negedge clock);
+      calculate_en = 1'b1;
+      // h0001.h0001
+      valueOne[fractionWidth+:wholeWidth] = 'd3;
+      valueOne[0+:fractionWidth] = 'd1137; // Need to conver this to hex, otherwise the hex fixed point number won't equal what is intended
+      // h0001.h0001
+      valueTwo[fractionWidth+:wholeWidth] = 'd16;
+      valueTwo[0+:fractionWidth] = 'd4173;
+      @(negedge clock);
+      calculate_en = 1'b0;
 
-    $display("");
-    //$display("0x%08h + 0x%08h = 0x%08h", valueOne, valueTwo, addend);
-    //$display("0x%08h - 0x%08h = 0x%08h", valueOne, valueTwo, difference);
-    $display("0x%08h * 0x%08h = 0x%08h", valueOne, valueTwo, product);
-    $display("%d.%05d * %d.%05d = %d.%05d", 
-      valueOne[fractionWidth+:wholeWidth], 
-      valueOne[0+:fractionWidth], 
-      valueTwo[fractionWidth+:wholeWidth], 
-      valueTwo[0+:fractionWidth],
-      product[fractionWidth+:wholeWidth], 
-      product[0+:fractionWidth]
-    );
+      $display("");
+      //$display("0x%08h + 0x%08h = 0x%08h", valueOne, valueTwo, addend);
+      //$display("0x%08h - 0x%08h = 0x%08h", valueOne, valueTwo, difference);
+      $display("0x%08h * 0x%08h = 0x%08h", valueOne, valueTwo, product);
+      $display("%d.%05d * %d.%05d = %d.%05d", 
+        valueOne[fractionWidth+:wholeWidth], 
+        valueOne[0+:fractionWidth], 
+        valueTwo[fractionWidth+:wholeWidth], 
+        valueTwo[0+:fractionWidth],
+        product[fractionWidth+:wholeWidth], 
+        product[0+:fractionWidth]
+      );
+    `endif
+
+    `ifdef TESTTASKS
+      real2hex(3.1415, working);
+    `endif
 
     #20ns;
 
